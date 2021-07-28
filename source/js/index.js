13a2d5c398b195a82dca581f28b5dbc7a04aefdb
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-07-28 17:30:09 +0200

    En cours

--- source/js/index.js
+++ source/js/index.js
@@ -1,4 +1,4 @@
-// Variable et constante 
+// Variable et constante ##################################
 let canvas = document.getElementById('myCanvas');
 let ctx = canvas.getContext('2d');
 
@@ -8,9 +8,22 @@ let y = canvas.height-30;
 let dx = 2;
 let dy = -2;
 
+// BALL
 let ballRadius = 10;
 
-// FONCTION
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width-paddleWidth) / 2;
+
+// RAQUETTE
+let rightPressed = false;
+let leftPressed = false;
+
+
+
+// FONCTION ################################################
+
+// DESSIN DE LA BALL
 function drawBall() {
 
     ctx.beginPath();
@@ -20,6 +33,19 @@ function drawBall() {
     ctx.closePath();
 
 };
+
+
+// RAQUETTE
+function drawPaddle() {
+
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+
+};
+
 
 // Dessin
 function draw() {
@@ -55,5 +81,42 @@ function draw() {
     y += dy;
 
 };
+
+
+// Listener
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+
+function keyDownHandler(e) {
+
+    if(e.key == "Right" || e.key == "ArrowRight") {
+
+        rightPressed = true;
+
+    }
+
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+
+        leftPressed = true;
+
+    };
+};
+
+function keyUpHandler(e) {
+
+    if(e.key == "Right" || e.key == "ArrowRight") {
+
+        rightPressed = false;
+
+    }
+
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+
+        leftPressed = false;
+
+    };
+};
+
 
 setInterval(draw, 10);

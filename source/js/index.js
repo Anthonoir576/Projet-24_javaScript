c5f02a5377157b40271b3c87ff22eda08f397796
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-08-01 21:31:35 +0200

    brique

--- source/js/index.js
+++ source/js/index.js
@@ -22,6 +22,15 @@ let paddleX = (canvas.width-paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
 
+// Brique
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+
 
 
 // FONCTION ################################################

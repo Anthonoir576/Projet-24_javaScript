b52642837d30c735f073a56e5762c3f9aa9c19b8
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-08-01 22:11:06 +0200

    base ok

--- source/js/index.js
+++ source/js/index.js
@@ -38,7 +38,7 @@ for(let c = 0; c < brickColumnCount; c++) {
 
     for(let r = 0; r < brickRowCount; r++) {
 
-        bricks[c][r] = { x: 0, y: 0 };
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
 
     }
 };
@@ -74,24 +74,27 @@ function drawPaddle() {
 // Brique 
 function drawBricks() {
 
-    for(let c = 0; c < brickColumnCount; c++) {
+    for(let c=0; c<brickColumnCount; c++) {
 
-        for(let r = 0; r < brickRowCount; r++) {
+        for(let r=0; r<brickRowCount; r++) {
 
-            let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-            let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+            if(bricks[c][r].status == 1) {
 
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
+                let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
 
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#0095DD";
-            ctx.fill();
-            ctx.closePath();
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+
+            }
         }
     }
-};
+}
 
 // Dessin
 function draw() {
@@ -101,6 +104,8 @@ function draw() {
     drawBricks();
     drawBall();
     drawPaddle();
+    collisionDetection();
+
 
 
 
@@ -205,4 +210,23 @@ function keyUpHandler(e) {
     };
 };
 
+// Fonction collision brique
+function collisionDetection() {
 
+    for(let c=0; c<brickColumnCount; c++) {
+
+        for(let r=0; r<brickRowCount; r++) {
+
+            let b = bricks[c][r];
+            if(b.status == 1) {
+
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+
+                    dy = -dy;
+                    b.status = 0;
+
+                }
+            }
+        }
+    }
+}
